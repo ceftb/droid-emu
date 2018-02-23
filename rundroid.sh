90655b0816a58396f54795367e7164f4210c7c7d
Lincoln Thurlow <lincoln@isi.edu>
2018-02-22 23:33:25 -0800

    auto select adb and console port

--- rundroid.sh
+++ rundroid.sh
@@ -9,4 +9,17 @@ if [[ $IMG = "" ]]; then
   echo "IMG environment variable required"
 fi
 
-./tools/emulator -avd $IMG -gpu off -skin 1080x1920 -no-window -noaudio -port 5554 -qemu -vnc :100
+# The valid ports range is 5554 to 5682
+firstport=5554
+lastport=5682
+counter=$firstport
+IP=localhost
+# lets assume allocation by 2's - adb low port, console high port
+for ((; counter<=$lastport; counter+=2)); do
+    if [[ ! $(netstat -nl | grep $counter) ]]; then
+        break
+    fi
+done
+echo "adb port selected:" $counter "console port:" $((counter+1))
+
+./tools/emulator -avd $IMG -gpu off -skin 1080x1920 -no-window -noaudio -ports $counter,$counter+1 -qemu -vnc :100
